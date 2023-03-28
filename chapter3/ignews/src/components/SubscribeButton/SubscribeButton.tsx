78d8b4911f4a5f32aefba300479b85385496fb38
Pedro Braga <pleao.braga@gmail.com>
2023-03-28 17:34:53 -0300

    feat: integrated with stribe

--- chapter3/ignews/src/components/SubscribeButton/SubscribeButton.tsx
+++ chapter3/ignews/src/components/SubscribeButton/SubscribeButton.tsx
@@ -1,3 +1,7 @@
+import { api } from '@/services/api'
+import { getStripeJS } from '@/services/stripe-js'
+import { signIn, useSession } from 'next-auth/react'
+
 import styles from './SubscribeButton.module.scss'
 
 interface SubscribeButtonProps {
@@ -5,10 +9,32 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { data: session } = useSession()
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn('github')
+      return
+    }
+
+    try {
+      const reponse = await api.post('/subscribe')
+
+      const { sessionId } = reponse.data
+
+      const stripe = await getStripeJS()
+
+      stripe?.redirectToCheckout({ sessionId })
+    } catch (err) {
+      alert(err.message)
+    }
+  }
+
   return (
     <button
       className={styles.subscribeButton}
       type="button"
+      onClick={handleSubscribe}
     >
       Subscribe now
     </button>
